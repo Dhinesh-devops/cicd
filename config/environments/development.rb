b558df9ca1433ad7057995c2bd21673a44aee70f
Karthick Nagarajan <karthick965938@gmail.com>
2023-07-12 11:26:00 +0530

    domain issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   config.assets.quiet = true
 
   config.force_ssl = true
+  config.hosts = "greenfuturz.spritle.com"
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
