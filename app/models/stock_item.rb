001cb9328a47ed2ea763cf6e620116a4263e05c0
Achyuth <achyuth@spritle.com>
2023-07-18 14:13:29 +0530

    Dashboard charts and UI changes

--- app/models/stock_item.rb
+++ app/models/stock_item.rb
@@ -6,6 +6,21 @@ class StockItem < ApplicationRecord
   scope :sold, -> { where(status: 2) }
   scope :with_rfid, -> { where.not(rfid_number: [nil, '']) }
   scope :without_rfid, -> { where(rfid_number: [nil, '']) }
+  scope :last_week, -> { where(updated_at: (Date.today - 7.days).beginning_of_day..Date.today.end_of_day) }
 
   enum status: {scanned: 1, sold: 2}
+
+  def self.get_chart_data(scanned_count, missed_count, sold_count)
+    seasons = StockItem.scanned.pluck(:season).uniq
+    season_with_count = seasons.map { |season| StockItem.scanned.where(season: season).size }
+
+    sizes = StockItem.scanned.pluck(:variant_size).uniq
+    size_count = sizes.map { |size| StockItem.scanned.where(variant_size: size).size }
+
+    sold_dates = StockItem.last_week.sold.pluck("date(stock_items.updated_at)").uniq
+    sold_format_dates = sold_dates.map { |sold_date| sold_date.strftime('%d-%m-%Y') }
+    sold_date_wise_count = sold_dates.map { |sold_date| StockItem.sold.where(:updated_at => sold_date.to_date.beginning_of_day..sold_date.to_date.end_of_day).size }
+
+    {:seasons => seasons, :season_with_count => season_with_count, :sizes => sizes, :size_count => size_count, :scanned_count => scanned_count, :missed_count => missed_count, :sold_count => sold_count, :sold_dates => sold_format_dates, :sold_date_wise_count => sold_date_wise_count }
+  end
 end
